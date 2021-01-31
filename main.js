24eac0c0867c7be8625821df2257a3d3d10ed2fd
mdbubbpsu <mdb5745@psu.edu>
2021-01-31 17:40:30 -0500

    big update

---
+++ main.js
@@ -0,0 +1,20 @@
+var goHome = document.getElementById('goHome')
+var goAboutMe = document.getElementById('goAboutMe')
+var goEd = document.getElementById('goEd')
+var goContact = document.getElementById('goContact')
+
+goHome.onclick = function(){
+    console.log("Go Home!")
+};
+
+goAboutMe.onclick = function(){
+    console.log("Go About Me!")
+};
+
+goEd.onclick = function(){
+    console.log("Go Education!")
+};
+
+goContact.onclick = function(){
+    console.log("Go Contact Me!")
+};
